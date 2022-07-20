2b268a41c876a03f1da41b85b931089e0fef8a18
hohihohi <hohihohi@shiraishihideakis-MacBook-Pro.local>
2022-07-20 13:38:26 +0900

    fix(install-ansible.sh): Fix how to judge pip package existance

--- install-ansible.sh
+++ install-ansible.sh
@@ -3,8 +3,8 @@
 
 # Definitions and variables
 readonly SHOULD_UPGRADE_PIP=false
-readonly SHOULD_UPGRADE_ANSIBLE=true
-readonly SHOULD_UPGRADE_ANSIBLE_LINT=true
+readonly SHOULD_UPGRADE_ANSIBLE=false
+readonly SHOULD_UPGRADE_ANSIBLE_LINT=false
 
 # Exit shell script immediately if failed and trace it
 set -xe
@@ -35,12 +35,11 @@ if test $? -ne 0; then
 fi
 ## To upgrade pip if you want
 if [ "${SHOULD_UPGRADE_PIP}" ]; then
-    echo 'Upgarde pip if possible'
+    echo 'Upgrade pip if possible'
     python3 -m ensurepip --upgrade >/dev/null
 fi
 ## Set systems python path
 if test $OS = 'Mac'; then
-    #python_version=$(ls "$HOME"/Library/Python/ | grep -e '3.*')
     python_version=$(find "$HOME"/Library/Python -type d -maxdepth 1 -not -name './*' | grep -o '[1-9].[1-9]*')
     export PATH="$HOME/Library/Python/${python_version}/bin:$PATH"
 else
@@ -49,30 +48,28 @@ fi
 
 # Installing Ansible-base
 ## To verify whether ansible-base is already installed on your preferred Python
-ansible --version >/dev/null 2>&1
-if test $? -ne 0; then
+if test $(python3 -m pip show ansible-base | grep -c 'ansible') = "0"; then
     ## To enable a user to execute ansible-base command if its command was not found
     echo 'Not found ansible command and install it'
-    python3 -m pip install --user ansible-base >/dev/null
+    python3 -m pip install --user ansible-base > /dev/null
 fi
 ## To upgrade ansible-base if you want
 if [ "${SHOULD_UPGRADE_ANSIBLE}" ]; then
     echo 'Upgrade ansible-base if possible'
-    python3 -m pip install --upgrade --user ansible-base >/dev/null
+    python3 -m pip install --upgrade --user ansible-base > /dev/null
 fi
 python3 -m pip list -v | grep ansible
 ansible --version
 
 # Installing Ansible-lint
-has_ansible_lint=$(python3 -m pip show ansible-lint | grep -c 'Name: ansible-lint')
-if test "$has_ansible_lint" -ne 1; then
+if test $(python3 -m pip show ansible-lint | grep -c 'Name: ansible-lint') = "0"; then
     ## To enable a user to execute ansible-lint command if its command was not found
     echo 'Not found ansible-lint command and install it'
-    python3 -m pip install --user ansible-lint >/dev/null
+    python3 -m pip install --user ansible-lint > /dev/null
 fi
 ## To upgrade ansible-lint if you want
 if [ "${SHOULD_UPGRADE_ANSIBLE_LINT}" ]; then
     echo 'Upgrade ansible-lint if possible'
-    python3 -m pip install --upgrade --user ansible-lint >/dev/null
+    python3 -m pip install --upgrade --user ansible-lint > /dev/null
 fi
 python3 -m pip show ansible-lint
